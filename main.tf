0110886ca4209713d5913ed9324982bfb3b50466
Isa Ishangulyyev <isa.ishangulyyev@gmail.com>
2025-10-07 22:52:48 +0200

    refactor: uncomment aws_instance resource and add output for instance hostname

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "vpc" {
   enable_dns_hostnames = true
 }
 
-/* resource "aws_instance" "app_server" {
+resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
@@ -37,4 +37,4 @@ module "vpc" {
   tags = {
     Name = var.instance_name
   }
-} */
+}
